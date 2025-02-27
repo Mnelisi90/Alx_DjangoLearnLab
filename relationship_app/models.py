2181eae792fe2f418829d8ef1798412248ddbe70
mnelisi90 <nmnelysn@gmail.com>
2025-02-27 21:12:55 +0200

    Added models in relationshipapp

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -1,29 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+    bio = models.TextField(blank=True)
 
     def __str__(self):
-        return self.name
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-class Library(models.Model):
-    name = models.CharField(max_length=200)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
