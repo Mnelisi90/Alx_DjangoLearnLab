3c07af41b884b8e78a36fdb375759b2d6ea765e8
mnelisi90 <nmnelysn@gmail.com>
2025-03-01 16:33:06 +0200

    Renamed author.first_name to author.name, removed last_name, and updated bio field

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    birth_date = models.DateField(null=True, blank=True)
-    bio = models.TextField(blank=True)
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.name
