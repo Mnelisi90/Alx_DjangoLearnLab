dfbd8b2349923122ef3d5d8dd1af35aeaad672f3
mnelisi90 <nmnelysn@gmail.com>
2025-03-02 14:07:08 +0200

    Add relationship app views

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -1,9 +1,16 @@
-# relationship_app/models.py
-from django.db import models 
+from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    bio = models.TextField()
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
